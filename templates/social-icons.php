fef8c8c8444b35e48b85793f515543e213c80623
RuslanKos1989 <arrusl@yandex.ru>
2022-07-17 13:50:52 +0300

    extend functionality for social buttons (hide on desktop)

--- templates/social-icons.php
+++ templates/social-icons.php
@@ -19,15 +19,20 @@ if ( ! $items ) {
 		<div class="rgbcode-menu-bottom-content__title"><?php echo esc_html( $title_ ); ?></div>
 	<?php } ?>
 	<div class="rgbcode-menu-bottom-content__items">
-		<?php foreach ( $items as $item ) { ?>
-			<div class="rgbcode-menu-bottom-content__item">
-				<a href="<?php echo esc_url( $item['link'] ); ?>" class="rgbcode-menu-bottom-content__link">
-					<img
-						class="rgbcode-menu-bottom-content__img"
-						src="<?php echo esc_url( $item['icon']['sizes']['thumbnail'] ?? '' ); ?>
-							"/>
-				</a>
-			</div>
-		<?php } ?>
+		<?php
+		foreach ( $items as $item ) :
+			if ( $item['hide_on_desktop'] ) {
+				continue;
+			}
+			?>
+		<div class="rgbcode-menu-bottom-content__item">
+			<a href="<?php echo esc_url( $item['link'] ); ?>" class="rgbcode-menu-bottom-content__link">
+				<img
+					class="rgbcode-menu-bottom-content__img"
+					src="<?php echo esc_url( $item['icon']['sizes']['thumbnail'] ?? '' ); ?>
+						"/>
+			</a>
+		</div>
+		<?php endforeach; ?>
 	</div>
 </div>
