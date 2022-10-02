681b6086046d593a538f3d42669f2e8ac05b6af6
RuslanKos1989 <arrusl@yandex.ru>
2022-10-02 16:10:05 +0300

    remove data for transfer to the theme

--- src/scripts/front/main.js
+++ src/scripts/front/main.js
@@ -3,7 +3,6 @@ import { initVideo } from './components/modalVideo';
 import { initMobileMenu } from './components/mobileMenu';
 import linksByPlatform from './components/linksByPlatform.js';
 import dynamicContent from './components/dynamicContent.js';
-import pandaForex from "./components/pandaForex.js";
 
 document.addEventListener( 'DOMContentLoaded', () => {
 	if ( ! document.getElementById( 'rgbcode-menu-header' ) ) {
@@ -15,5 +14,4 @@ document.addEventListener( 'DOMContentLoaded', () => {
 	initVideo();
 	linksByPlatform();
 	dynamicContent();
-	pandaForex();
 } );
